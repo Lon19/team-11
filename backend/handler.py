c22620020c9efea60758a86496617c50f5b669bf
Jay Carder <2jaycarder5@gmail.com>
2019-11-01 21:38:08 +0000

    basic hello world communication between back end to front end

--- backend/handler.py
+++ backend/handler.py
@@ -20,6 +20,7 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 @app.route('/hello', methods=['GET'])
 def hello_world():
     return jsonify({'res':'Hello, World!'})
--- frontend/mtw/src/Main.js
+++ frontend/mtw/src/Main.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './Main.css';
+import axios from 'axios'
+
+const API_URL = 'http://localhost:5000/hello'
+
+export function fetch() {
+  return axios.get(`${API_URL}`)
+}
 
 function Main() {
+
+  const [msg, setMsg] = useState("hello");
+
+  const componentDidMount = () => {
+    axios.get(API_URL)
+    .then(response => {
+      console.log(response.data);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  }
+
+
   return (
     <div className="Main">
       <header className="Main-header">
         <img src={logo} className="Main-logo" alt="logo" />
+        {componentDidMount()}
         <p>
           Edit <code>src/Main.js</code> and save to reload.
         </p>
