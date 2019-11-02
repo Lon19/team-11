4aaec9fcbb8cd95fc5759861110130bf5f5243ad
iPata <y.pata@warwick.ac.uk>
2019-11-02 02:21:21 +0000

    Historical data retrieved for specified wards (24 months)

--- backend/fetch_data.py
+++ backend/fetch_data.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import requests
+from flask import jsonify
 
 api_nomis = pd.read_csv('./backend/nomis_data.csv').sort_values(by = ['GEOGRAPHY_NAME'])
 
@@ -12,5 +13,38 @@ api_nomis = api_nomis.rename(columns={"final_total" : "total"})
 def get_data():
     return api_nomis.to_json(orient='index')
 
+# def download_hist_data():
+#     for i in range(0, 24):
+#         current_df = pd.read_csv('http://www.nomisweb.co.uk/api/v01/dataset/NM_162_1.data.csv?' +
+#                                  'date=latestMINUS' + str(i) + '&' +
+#                                  'geography=2092957697TYPE236&' +
+#                                  'uid=&' + #!!!!ADD THE KEY FROM THE JS FILE HERE!!!!
+#                                  'age=11,12&' +
+#                                  'gender=0&' +
+#                                  'select=geography_code,geography_name,gender_name,obs_value&' +
+#                                  'measure=1&' + 
+#                                  'rows=geography_code,geography_name&' +
+#                                  'cols=gender_name&' + 
+#                                  'measures=20100')
+        
+#         current_df['final_total'] = current_df['Total'] + current_df['Total.1']
+#         current_df.drop(['GEOGRAPHY_NAME', 'Total', 'Total.1'], axis = 1, inplace = True)
+#         current_df.to_csv('./backend/latest_minus_' + str(i) + '_months.csv')
+#         return None
+
 def get_ward_hist(ward):
-    return None
+    composite_df = pd.read_csv('./backend/latest_minus_0_months.csv')['GEOGRAPHY_CODE']
+    for i in range(0, 24):
+        temp_df = pd.read_csv('./backend/latest_minus_' + str(i) + '_months.csv', index_col = 0).drop(['GEOGRAPHY_CODE'], axis = 1)
+        temp_df.columns = [str(i)]
+        composite_df = pd.concat([composite_df, temp_df], axis=1, sort=False)
+    composite_df = composite_df[composite_df['GEOGRAPHY_CODE'] == ward]
+    composite_np = np.flip(np.asarray(composite_df))[0][0:-1]
+
+    # for i in range(0, 24):
+    #     composite_df[str(i)] = composite_np[i]
+    # composite_df = composite_df.set_index(keys = ['GEOGRAPHY_CODE'])
+    # return composite_df.to_json(orient = 'values')
+    return jsonify({ward:jsonify(composite_np)})
+
+print(get_ward_hist('95AA01'))
