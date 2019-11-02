d92c3f6ac0f5421568425d5c068657b60e2e0b92
Joseph Bond <jecb1998@googlemail.com>
2019-11-02 00:08:53 +0000

    Second most pointless commmit in history

--- frontend/mtw/src/Main.js
+++ frontend/mtw/src/Main.js
@@ -2,7 +2,7 @@ import React from 'react';
 import MtwNav from './MtwNav.js';
 import Map from './Map';
 import './Main.css';
-
+import {Button, Modal, ModalReader, ModalBody, ModalFooter} from 'reactstrap'
 function Main() {
   return (
     <div className="Main">
