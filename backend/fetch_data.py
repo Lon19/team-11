c3db97c4b3e7fe57e8c1fdab38822cd620418587
Scott Pickering <scottdpickering@gmail.com>
2019-11-02 11:22:43 +0000

    Remove comments

--- backend/fetch_data.py
+++ backend/fetch_data.py
@@ -19,15 +19,6 @@ def get_old_ward(new_ward):
 new_ward_data = pd.read_csv('./backend/new_ward_data.csv')
 old_ward_data = pd.read_csv('./backend/old_ward_data.csv')
 
-# api_nomis['final_total'] = api_nomis['Total'] + api_nomis['Total.1']
-# api_nomis = (api_nomis.drop(['Female', 'Female.1', 'Male', 'Male.1', 'Total', 'Total.1', 'GEOGRAPHY_NAME'], axis = 1))
-# for i in range(0, len(new_ward_data)):
-#     print(get_new_ward(new_ward_data['GEOGRAPHY_CODE'][i])[0])
-#     new_ward_data['GEOGRAPHY_CODE'][i] = get_new_ward(new_ward_data['GEOGRAPHY_CODE'][i])[0]
-#     if i % 1000 == 0:
-#         print(i)
-
-# print(new_ward_data.head())
 new_ward_data = new_ward_data.set_index(verify_integrity = True, keys = ['GEOGRAPHY_CODE'])
 new_ward_data = new_ward_data.rename(columns={"final_total" : "total"})
 old_ward_data = old_ward_data.set_index(verify_integrity = True, keys = ['GEOGRAPHY_CODE'])
@@ -39,25 +30,6 @@ def get_data():
 def get_data_old():
     return old_ward_data.to_json(orient='index')
 
-# def download_hist_data():
-#     for i in range(0, 24):
-#         current_df = pd.read_csv('http://www.nomisweb.co.uk/api/v01/dataset/NM_162_1.data.csv?' +
-#                                  'date=latestMINUS' + str(i) + '&' +
-#                                  'geography=2092957697TYPE236&' +
-#                                  'uid=&' + #!!!!ADD THE KEY FROM THE JS FILE HERE!!!!
-#                                  'age=11,12&' +
-#                                  'gender=0&' +
-#                                  'select=geography_code,geography_name,gender_name,obs_value&' +
-#                                  'measure=1&' + 
-#                                  'rows=geography_code,geography_name&' +
-#                                  'cols=gender_name&' + 
-#                                  'measures=20100')
-        
-#         current_df['final_total'] = current_df['Total'] + current_df['Total.1']
-#         current_df.drop(['GEOGRAPHY_NAME', 'Total', 'Total.1'], axis = 1, inplace = True)
-#         current_df.to_csv('./backend/latest_minus_' + str(i) + '_months.csv')
-#         return None
-
 def get_ward_hist(ward):
     composite_df = pd.read_csv('./backend/latest_minus_0_months.csv')['GEOGRAPHY_CODE']
     for i in range(0, 24):
@@ -67,10 +39,6 @@ def get_ward_hist(ward):
     composite_df = composite_df[composite_df['GEOGRAPHY_CODE'] == ward]
     composite_np = np.flip(np.asarray(composite_df))[0][0:-1]
 
-    # for i in range(0, 24):
-    #     composite_df[str(i)] = composite_np[i]
-    # composite_df = composite_df.set_index(keys = ['GEOGRAPHY_CODE'])
-    # return composite_df.to_json(orient = 'values')
     return json.dumps({ward:composite_np.tolist()})
 
 
