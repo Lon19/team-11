97165a7603a259e659edff1793189ba0733fb1db
Jay Carder <2jaycarder5@gmail.com>
2019-11-02 01:15:19 +0000

    format box sizes

--- frontend/mtw/src/Map.js
+++ frontend/mtw/src/Map.js
@@ -1,18 +1,23 @@
 import React from 'react';
-import { Card, CardTitle, Row, Col } from 'reactstrap';
+import { Container, Card, CardTitle, Row, Col } from 'reactstrap';
 import './Map.css';
 
 function Map() {
     return (
-        <Row>
-            <div className="MapBox">
-                <Col xs='12' md='12' lg='12'>
-                    <Card body>
+        <Container style={{margin:"0", minWidth: '100%', height: '100%'}}>
+            <Row style={{height: '100%'}}>
+                <Col style={{paddingLeft: "15px", paddingRight: "8px", height: '100%'}} xs="8" md="8" lg="8">
+                    <Card className="Mapboi">
                         <CardTitle>This map gon be lit y'all</CardTitle>
                     </Card>
                 </Col>
-            </div>
-        </Row>
+                <Col style={{paddingLeft: "8px", paddingRight: "15px", height: '100%'}} xs="4" md="4" lg="4">
+                    <Card className="Otherboi">
+                        <CardTitle>This map gon be lit y'all</CardTitle>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
     );
 }
 
--- frontend/mtw/src/MtwNav.js
+++ frontend/mtw/src/MtwNav.js
@@ -9,8 +9,7 @@ function MtwNav() {
     return (
         <Navbar style={{backgroundColor: 'white', borderBottom: '1px solid #dfdfdf'}} light expand="md">
             <NavbarBrand style={{color: '#7b7b7b'}} href="/">MTW Map</NavbarBrand>
-            <Nav className="ml-auto" navbar>
-            </Nav>
+            <Nav className="ml-auto" navbar></Nav>
         </Navbar>
     );
   }
