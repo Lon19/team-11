9d058ee22875d1dc2d6f44be75ffdfb27dc40d49
iPata <y.pata@warwick.ac.uk>
2019-11-02 07:20:15 +0000

    Typo2

--- backend/fetch_data.py
+++ backend/fetch_data.py
@@ -10,12 +10,12 @@ def get_new_ward(old_ward):
 
     return np.asarray(ward_lookup[ward_lookup['old_ward'] == old_ward]['new_ward'])
 
-api_nomis = pd.read_csv('./backend/fixed_data.csv').sort_values(by = ['GEOGRAPHY_NAME'])
+api_nomis = pd.read_csv('./backend/fixed_data.csv')
 
-api_nomis['final_total'] = api_nomis['Total'] + api_nomis['Total.1']
-api_nomis = (api_nomis.drop(['Female', 'Female.1', 'Male', 'Male.1', 'Total', 'Total.1', 'GEOGRAPHY_NAME'], axis = 1))
-api_nomis = api_nomis.set_index(verify_integrity = True, keys = ['GEOGRAPHY_CODE'])
-api_nomis = api_nomis.rename(columns={"final_total" : "total"})
+# api_nomis['final_total'] = api_nomis['Total'] + api_nomis['Total.1']
+# api_nomis = (api_nomis.drop(['Female', 'Female.1', 'Male', 'Male.1', 'Total', 'Total.1', 'GEOGRAPHY_NAME'], axis = 1))
+# api_nomis = api_nomis.set_index(verify_integrity = True, keys = ['GEOGRAPHY_CODE'])
+# api_nomis = api_nomis.rename(columns={"final_total" : "total"})
 
 def get_data():
     return api_nomis.to_json(orient='index')
